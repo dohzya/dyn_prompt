5b7f2aaa169591eafa143fb88a2b5894d97620ee
Etienne Vallette d'Osia <dohzya@gmail.com>
2009-10-05 09:29:16 +0200

    prompt: fix rvm for last version

--- parsers/rvm.rb
+++ parsers/rvm.rb
@@ -1,12 +1,12 @@
 class RVMParser < DynPrompt::Parser::SCM
     def self.active?
-      ENV.has_key? 'rvm_prompt'
+      ENV.has_key? 'rvm_ruby_version'
     end
 
   # parsers
 
   def parse_version
-    ENV['rvm_prompt']
+    ENV['rvm_ruby_version']
   end
 
   # end of parsers
